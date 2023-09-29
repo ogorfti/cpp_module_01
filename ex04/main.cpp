fd04c8a9ae2ab85f9e4bc7f0500ba20fccca681f
ogorfti <ww.shanks1@gmail.com>
2023-09-29 20:29:16 +0100

    ex04 done

---
+++ ex04/main.cpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ogorfti <ogorfti@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/29 15:25:56 by ogorfti           #+#    #+#             */
+/*   Updated: 2023/09/29 20:27:01 by ogorfti          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+
+int main(int ac, char **av)
+{
+	std::string	name;
+	std::string	s1;
+	std::string	s2;
+
+	if (ac == 4)
+	{
+		s2 = av[3];
+		s1 = av[2];
+		name = av[1];
+		std::fstream sourceFile(name);
+		std::ofstream destFile(name + ".replace");
+		if (sourceFile.is_open() && destFile.is_open())
+		{
+			std::string line;
+			while (std::getline(sourceFile, line, '\0'))
+			{
+				size_t pos = 0;
+				while ((pos = line.find(s1, pos)) != std::string::npos)
+				{
+					line.erase(pos, s1.length());
+					line.insert(pos, s2);
+					pos += s2.length();
+				}
+				destFile << line;
+			}
+			destFile.close();
+			sourceFile.close();
+		}
+		else
+			std::cerr << "Error opening files" << std::endl;
+	}
+}
